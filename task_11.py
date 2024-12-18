eabce5d603c4e0fa527e3e7e2a86b4a493038904
Jason van Hamond <232567@buas.nl>
2024-12-18 13:24:05 +0100

    Changes to timesteps task 11

--- task_11.py
+++ task_11.py
@@ -37,7 +37,7 @@ env = OT2Env()
 run = wandb.init(project="task11",sync_tensorboard=True)
 
 # Set the amount of epochs for the model to learn
-timesteps = 1000000
+timesteps = 6000000
 # Define the arguments
 parser = argparse.ArgumentParser()
 # Set the default parameters.
@@ -63,7 +63,7 @@ model = PPO(args.policy, env, verbose=1,
             tensorboard_log=f"runs/{run.id}")
 
 wandb_callback = WandbCallback(
-    model_save_freq=1000,
+    model_save_freq=100000,
     model_save_path=f"models/{run.id}",
     verbose=2,)
 
